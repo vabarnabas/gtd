5ba32759da3a9607d76543b435b2b1fb41f4c537
Barnabas Varga <vabarnabas@gmail.com>
2022-12-13 13:32:24 +0100

    Test

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -4,7 +4,7 @@ import useModalStore from "../../store/modal.store"
 import ChangeFolderModal from "../change-folder-modal"
 import ConfirmModal from "../confirm-modal"
 import Navbar from "../navbar"
-import NewTaskModal from "../new-task-modal"
+import NewFolderModal from "../new-folder-modal"
 import Toast from "../toast"
 import ToastHandler from "../toast/toast-handler"
 
@@ -45,16 +45,18 @@ export default function Layout({ children, fetchTasks, fetchFolders }: Props) {
           }
         />
       ) : null}
-      {currentModal.modal === "new-task" ? (
-        <NewTaskModal isOpen={currentModal.modal === "new-task"} />
-      ) : null}
       {/* 
       {currentModal.modal === "new-task" ? (
         <NewTaskModal
           fetchTasks={fetchTasks && fetchTasks}
           isOpen={currentModal.modal === "new-task"}
         />
+      ) : null} */}
+      {currentModal.modal === "new-folder" ? (
+        <NewFolderModal isOpen={currentModal.modal === "new-folder"} />
       ) : null}
+      {/* 
+      
       {currentModal.modal === "new-folder" ? (
         <NewFolderModal
           fetchFolders={fetchFolders && fetchFolders}
