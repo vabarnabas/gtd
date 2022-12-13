c6b7601261a64254f12afa73b727f2d3d3010b06
Barnabas Varga <vabarnabas@gmail.com>
2022-12-13 13:25:18 +0100

    Folder

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 
 import useModalStore from "../../store/modal.store"
-import ChangePasswordModal from "../change-password-modal"
+import ChangeFolderModal from "../change-folder-modal"
 import ConfirmModal from "../confirm-modal"
 import Navbar from "../navbar"
 import Toast from "../toast"
@@ -30,9 +30,18 @@ export default function Layout({ children, fetchTasks, fetchFolders }: Props) {
       {currentModal.modal === "confirm" && currentModal.action ? (
         <ConfirmModal isOpen={currentModal.modal === "confirm"} />
       ) : null}
-      {currentModal.modal === "change-password" ? (
+      {/* {currentModal.modal === "change-password" ? (
         <ChangePasswordModal
           isOpen={currentModal.modal === "change-password"}
+        />
+      ) : null} */}
+      {currentModal.modal === "change-folder" &&
+      currentModal.id !== undefined ? (
+        <ChangeFolderModal
+          isOpen={
+            currentModal.modal === "change-folder" &&
+            currentModal.id !== undefined
+          }
         />
       ) : null}
       {/* 
