03e10963fe62ebdc1016d547881a48f996d1dd13
Barnabas Varga <vabarnabas@gmail.com>
2022-12-13 12:33:27 +0100

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useEffect, useState } from "react"
 import useSWR from "swr"
 
 import FolderRow from "../components/folder-row"
@@ -6,24 +5,11 @@ import Layout from "../components/layout"
 import Spinner from "../components/spinner"
 import { FolderHelper } from "../helpers/FolderHelper"
 import { requestHelper } from "../services/requestHelper"
-import TokenService from "../services/token.service"
 import { useErrorHandler } from "../services/useErrorHandler"
 import { Folder } from "../types/prisma.types"
 
 export default function Home() {
   const { errorHandler } = useErrorHandler()
-
-  const tokenservice = new TokenService()
-  const [folders, setFolders] = useState<Folder[]>([])
-  const fetchFolders = useCallback(() => {
-    errorHandler(async () => {
-      const data = await requestHelper.getMy<Folder>("folders")
-    })
-  }, [errorHandler])
-
-  useEffect(() => {
-    fetchFolders()
-  }, [fetchFolders])
 
   const { data, error, isLoading } = useSWR(
     "fetchFolders",
@@ -31,7 +17,7 @@ export default function Home() {
   )
 
   return (
-    <Layout fetchFolders={fetchFolders}>
+    <Layout>
       <div className="flex h-full w-full flex-col items-center rounded-md px-4 pt-4 pb-2 shadow">
         {!error ? (
           !isLoading && data ? (
