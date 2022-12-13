8ec8334a77ce977a65be2cd9e4ba8999f5212c43
Barnabas Varga <vabarnabas@gmail.com>
2022-12-13 12:31:10 +0100

    Test

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,28 @@
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
+
+import { verify } from "./services/verifyToken"
 
 export async function middleware(req: NextRequest) {
-  // const token = req.cookies.get("gtd-token")
-  // if (req.nextUrl.pathname.startsWith("/login")) {
-  //   if (token?.value === undefined || token.value === "undefined") {
-  //     req.cookies.delete("gtd-token")
-  //   }
-  //   return NextResponse.next()
-  // }
-  // if (req.nextUrl.pathname.startsWith("/_next")) {
-  //   return NextResponse.next()
-  // }
-  // if (!req.nextUrl.pathname.startsWith("/login")) {
-  //   if (token?.value === undefined || token.value === "undefined") {
-  //     req.cookies.delete("gtd-token")
-  //     return NextResponse.redirect(new URL("/login", req.url))
-  //   }
-  //   try {
-  //     await verify(token.value, "at-secret")
-  //     return NextResponse.next()
-  //   } catch (error) {
-  //     return NextResponse.redirect(new URL("/login", req.url))
-  //   }
-  // }
+  const token = req.cookies.get("gtd-token")
+  if (req.nextUrl.pathname.startsWith("/login")) {
+    if (token?.value === undefined || token.value === "undefined") {
+      req.cookies.delete("gtd-token")
+    }
+    return NextResponse.next()
+  }
+  if (req.nextUrl.pathname.startsWith("/_next")) {
+    return NextResponse.next()
+  }
+  if (!req.nextUrl.pathname.startsWith("/login")) {
+    if (token?.value === undefined || token.value === "undefined") {
+      req.cookies.delete("gtd-token")
+      return NextResponse.redirect(new URL("/login", req.url))
+    }
+    try {
+      await verify(token.value, "at-secret")
+      return NextResponse.next()
+    } catch (error) {
+      return NextResponse.redirect(new URL("/login", req.url))
+    }
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import { useCallback, useEffect, useState } from "react"
+import useSWR from "swr"
 
+import FolderRow from "../components/folder-row"
 import Layout from "../components/layout"
+import Spinner from "../components/spinner"
+import { FolderHelper } from "../helpers/FolderHelper"
 import { requestHelper } from "../services/requestHelper"
 import TokenService from "../services/token.service"
 import { useErrorHandler } from "../services/useErrorHandler"
@@ -14,7 +18,6 @@ export default function Home() {
   const fetchFolders = useCallback(() => {
     errorHandler(async () => {
       const data = await requestHelper.getMy<Folder>("folders")
-      setFolders(data)
     })
   }, [errorHandler])
 
@@ -22,24 +25,28 @@ export default function Home() {
     fetchFolders()
   }, [fetchFolders])
 
+  const { data, error, isLoading } = useSWR(
+    "fetchFolders",
+    async () => await requestHelper.getMy<Folder>("folders")
+  )
+
   return (
     <Layout fetchFolders={fetchFolders}>
-      {/* <div className="flex h-full w-full flex-col items-center rounded-md px-4 pt-4 pb-2 shadow">
-        {folders.length !== 0 ? (
-          <div className="w-full rounded-md bg-white p-4 shadow-md">
-            <p className="mb-3 text-lg font-bold">My Folders</p>
-            {FolderHelper.findTopLevel(folders).map((folder) => (
-              <FolderRow key={folder.id} folders={folders} folder={folder} />
-            ))}
-          </div>
+      <div className="flex h-full w-full flex-col items-center rounded-md px-4 pt-4 pb-2 shadow">
+        {!error ? (
+          !isLoading && data ? (
+            <div className="w-full rounded-md bg-white p-4 shadow-md">
+              <p className="mb-3 text-lg font-bold">My Folders</p>
+              {FolderHelper.findTopLevel(data).map((folder) => (
+                <FolderRow key={folder.id} folders={data} folder={folder} />
+              ))}
+            </div>
+          ) : (
+            <Spinner />
+          )
         ) : (
-          <Spinner />
+          <div className="">Something went wrong.</div>
         )}
-      </div> */}
-
-      <div className="">
-        <p className="">{"folders: " + JSON.stringify(folders)}</p>
-        <p className="">{"token: " + tokenservice.getToken()}</p>
       </div>
     </Layout>
   )
